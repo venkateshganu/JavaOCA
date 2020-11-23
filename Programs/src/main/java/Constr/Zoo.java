7498488b40123dfd8aff50783d991b5d9c7b137a
Venkatesh Gotimukul <venkatesh.gotimukul1@t-mobile.com>
2020-11-23 13:01:28 -0800

    Trial started 2

---
+++ Programs/src/main/java/Constr/Zoo.java
@@ -0,0 +1,11 @@
+package Constr;
+
+public class Zoo {
+    int[] a = {1,2,3,4};
+    public static void main(String[] args) {
+
+        Zoo z= new Zoo();
+        System.out.println(z.a[0]);
+        System.out.println(z.a[1]);
+    }
+}
